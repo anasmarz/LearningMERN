303f8f529ece29578811ef45996e1d5e974c04fd
AnasUpm <muhammadanasmarz@gmail.com>
2024-07-08 16:31:35 +0800

    backend test

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { PORT } from "./config.js";
+const app = express();
+
+app.get('/', (request,response) => {
+    console.log(request)
+    return response.status(234).send('Welcome')
+});
+
+app.listen(PORT, () => {
+    console.log(`app is listening on port: ${PORT}`);
+});
